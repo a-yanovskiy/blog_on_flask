48542ea659a9dffdff4b0f165884da2bd0feee91
a-yanovskiy <a.e.yanovskiy@gmail.com>
2022-08-23 22:36:33 +0300

    add: post detail with slug

--- app/posts/blueprint.py
+++ app/posts/blueprint.py
@@ -9,3 +9,9 @@ def index():
     posts_list = Post.query.all()
     return render_template('posts/index.html',
                            posts=posts_list)
+
+
+@posts.route('/<slug>')
+def post_detail(slug):
+    post_by_slug = Post.query.filter(Post.slug == slug).first()
+    return render_template('posts/post_detail.html', post=post_by_slug)
